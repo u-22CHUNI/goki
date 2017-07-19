4031a791520288b601f74b22916ac1dfbc89088b
ぼく <kbc16a09@stu.kawahara.ac.jp>
2017-07-19 15:58:43 +0900

    さいしょ

---
+++ calltest2.php
@@ -0,0 +1,24 @@
+﻿
+<?php
+header("Content-type: text/html");
+
+//call side
+
+$fullPath ='python C:/xampp/htdocs/u-22apitest/testWatson.py';
+//$fullPath ='python /home/boku/callpythontest.py'; //vps
+
+header( "Content-Type: application/json; charset=utf-8" );
+exec($fullPath,$outpara);//外部コマンドを実行
+//print_r ($outpara);
+
+$outjson=json_decode(implode($outpara),true);
+
+$kni=$outjson["images"][0]["classifiers"][0]["classes"];
+
+foreach ($kni as $key => $value ){
+
+    print "class :".$value["class"]."\n";
+    print "score :".$value["score"]."\n";
+}
+
+?>
---
+++ testWatson.py
@@ -0,0 +1,18 @@
+﻿import json
+from os.path import join, dirname
+from watson_developer_cloud import VisualRecognitionV3
+
+# API Keyを入力する
+visual_recognition = VisualRecognitionV3('', api_key='')
+
+#throwfile = '/home/boku/.testFolder/KNI.jpg
+throwfile ='C:/xampp/htdocs/u-22apitest/KNI.jpg'
+
+# ローカルにあるファイルを使う場合
+def watsonViRe():
+# 何かわからないものを認識させたい時
+    with open(join(dirname(__file__),throwfile), 'rb') as image_file:
+        scanfile=visual_recognition.classify(images_file=image_file)
+        print(json.dumps(scanfile, indent=2))
+        
+watsonViRe()
